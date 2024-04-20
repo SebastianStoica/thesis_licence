48a183d65819331aedf4b6aadcfe2e9c95298d79
SebastianStoica <seby.stoica28@gmail.com>
2024-04-20 13:23:29 +0300

    TF AND OBSTACLE AND CONTROL WOTH TWIST

--- build/licenta/build/lib/licenta/obstacle_avoider.py
+++ build/licenta/build/lib/licenta/obstacle_avoider.py
@@ -1,30 +1,32 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Range
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 MAX_RANGE = 0.15
+MAX_LINEAR_SPEED = 0.5
+MAX_ANGULAR_SPEED = 2.0  
 
 
 class ObstacleAvoider(Node):
     def __init__(self):
         super().__init__('obstacle_avoider')
-        self.lidar_data = LaserScan()
-        self.create_subscription(LaserScan, 'lidar_sensor', self.lidar_sensor_callback, 10)
-        self.create_timer(1, self.step_obstclate)
 
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10) 
+        #
+        self.lidar_msg = LaserScan()
+        self.create_subscription(LaserScan, 'lidar_sensor', self.lidar_sensor_callback, 10)
+        r = self.lidar_msg.ranges
 
     def lidar_sensor_callback(self,msg):
-        self.lidar_data = msg
-    
-    def step_obstclate(self):
-        data = self.lidar_data.ranges
-      
+        self.lidar_msg = msg
+        
 
 def main(args=None):
     rclpy.init(args=args)
+
     avoider = ObstacleAvoider()
+
     rclpy.spin(avoider)
 
     avoider.destroy_node()
---
+++ install/licenta/share/licenta/launch/robot_launch.py
@@ -0,0 +1,44 @@
+import os
+import pathlib
+import launch
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher,Ros2SupervisorLauncher
+from webots_ros2_driver.utils import controller_url_prefix
+ros2 = Ros2SupervisorLauncher()
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('licenta')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'khepera.urdf')).read_text()
+
+   # webots = WebotsLauncher(  world=os.path.join(package_dir, 'worlds', 'world_new_obstcl.wbt')  )
+
+
+    my_robot_driver = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',
+        additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix() + 'Khepera'},
+        parameters=[{'robot_description': robot_description},
+                     {'webots_ros2_driver': {'controller_name': 'Khepera'}}
+                    ]
+    )
+    obstacle_avoider = Node(
+        package='licenta',
+        executable='obstacle_avoider',
+    )
+
+
+    return LaunchDescription([
+        #webots,
+        my_robot_driver,
+        #obstacle_avoider,
+        #launch.actions.RegisterEventHandler(
+            #event_handler=launch.event_handlers.OnProcessExit(
+                #target_action=webots,
+               # on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            #)
+       # )
+    ])
